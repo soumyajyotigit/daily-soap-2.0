3c6c486e1b1d6f1cb4b225ae81a4891863ede5be
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2021-09-26 13:29:01 +0530

    Create Element Search in a Matrix.cpp

---
+++ Element Search in a Matrix.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    cout<<"matrix is:"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int x;
+    cin>>x;
+    bool flag=false;
+    for (int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(arr[i][j]==x){
+                cout<<i<<" "<<j<<endl;
+                flag=true;
+            }
+        }
+    }
+    if (flag){
+        cout<<"element found";
+    }
+    else{
+        cout<<"element not found";
+    }
+
+    return 0;
+}
